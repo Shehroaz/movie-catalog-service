10bdfe19fe56f3dc2842ef8c63e9650c15310b61
Shehroaz <muhammadshehroaz@gmail.com>
2021-02-08 16:14:32 +0500

    calling microservice movie-info by simple logic not using lambdas

--- src/main/java/com/sherry/moviecatalogservice/controllers/MovieCatalogResources.java
+++ src/main/java/com/sherry/moviecatalogservice/controllers/MovieCatalogResources.java
@@ -8,11 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -23,27 +21,24 @@ public class MovieCatalogResources {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        List<CatalogItem> list  = new ArrayList<>();
 
-        //get all related movie ID'S
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234" , 4),
                 new Rating("1235" , 3)
         );
-        //for each movie ID, get call info service and get details
-       return ratings.stream().map(rating -> {
-          Movie movie =  restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId() , Movie.class);
-           return new CatalogItem(movie.getName() , "see it" ,rating.getRating());
-               })
-               .collect(Collectors.toList());
 
-        //put them all together
-        /*
-        in this we create hard coded ratings then we call though RestTemplate the other microservice
-        and getting the response the RestTemplate unmarshall the String into required object
-         */
+        for (Rating rating : ratings){
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId() , Movie.class);
+            list.add(new CatalogItem(movie.getName() , "Mazy" , rating.getRating()));
+        }
+        return list;
 
-
-
+//       return ratings.stream().map(rating -> {
+//          Movie movie =  restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId() , Movie.class);
+//           return new CatalogItem(movie.getName() , "see it" ,rating.getRating());
+//               })
+//               .collect(Collectors.toList());
 
     }
 
