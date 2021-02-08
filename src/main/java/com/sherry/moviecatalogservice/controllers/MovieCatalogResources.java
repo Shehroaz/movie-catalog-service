b5d5628a9d223970c3a431457d783219605d3e50
Shehroaz <muhammadshehroaz@gmail.com>
2021-02-08 16:56:45 +0500

    making list as a bean so it is to instantiate again,

--- src/main/java/com/sherry/moviecatalogservice/controllers/MovieCatalogResources.java
+++ src/main/java/com/sherry/moviecatalogservice/controllers/MovieCatalogResources.java
@@ -8,10 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.lang.ref.WeakReference;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -22,8 +20,7 @@ public class MovieCatalogResources {
     private RestTemplate restTemplate;
 
     @Autowired
-    private WebClient.Builder webClientBuilder;
-
+    private List<CatalogItem> list;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
@@ -33,13 +30,17 @@ public class MovieCatalogResources {
                 new Rating("1235" , 3)
         );
 
+        for (Rating rating : ratings){
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId() , Movie.class);
+            list.add(new CatalogItem(movie.getName() , "see on your choice" , rating.getRating()));
+        }
+        return list;
 
-
-       return ratings.stream().map(rating -> {
-          Movie movie =  restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId() , Movie.class);
-           return new CatalogItem(movie.getName() , "see it" ,rating.getRating());
-               })
-               .collect(Collectors.toList());
+//       return ratings.stream().map(rating -> {
+//          Movie movie =  restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId() , Movie.class);
+//           return new CatalogItem(movie.getName() , "see it" ,rating.getRating());
+//               })
+//               .collect(Collectors.toList());
 
     }
 
